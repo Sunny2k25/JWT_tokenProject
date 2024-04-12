308f465a2bbd714137786366a53ad0ad24a9e0e8
Sunny Jaiswal <jaiswalsunny055@gmail.com>
2024-04-13 03:07:31 +0530

    Project Done

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,51 +1,50 @@
 const { response } = require("express");
-const userModel = require("../models/userModel")
+const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const middleware = require("../Middleware/verifyMiddleware")
 
-const jwt = require('jsonwebtoken')
+const handleUserSignUp = (req, res) => {
+  try {
+    userModel
+      .create(req.body)
+      .then(() => {
+        res.json({ Message: "Created Sucessfully" }).status(201);
+      })
+      .catch((err) => {
+        res.json({ Message: "This is Wrong", err: err }).status(500);
+      });
+  } catch (err) {
+    res.json({ message: "This is wrong", err: err }).status(500);
+  }
+};
+
+const handleUserLogin = (req, res) => {
+  let { email } = req.body; //destruction of body
+  try {
+    userModel.find({ email: email }).then((response) => {
+      if (response.length >= 1) {
+        jwt.sign(req.body, process.env.SECRET_KEY, (err, token) => {
+          if (err) {
+            res.json({ message: "This is wrong", err: err }).status(500);
+          } else {
+            res.json({
+              Message: "Login Sucessfull",
+              data: req.body,
+              token: token,
+            });
+          }
+        });
+      }
+    });
+  } catch (err) {
+    res.json({ message: "This is error", err: err }).status(500);
+  }
+};
 
 
-const handleUserSignUp = (req,res)=>{
-    try{
-    userModel.create(req.body).then(()=>{
-        res.json({"Message":"Created Sucessfully"}).status(201);
+const getdetails =(req,res)=>{
+  console.log(req.email);
 
-    }).catch((err)=>{
-        res.json({"Message": "This is Wrong",err:err}).status(500);
-    })
 }
-catch(err){
-    res.json({"message":"This is wrong",err:err}).status(500)
-}
-}
 
-
-
-
-const handleUserLogin = (req,res)=>{
-    let {email} =req.body;   //destruction of body
-    try{
-    userModel.find({email:email}).then((response)=>{
-        if(response.length>=1){
-            jwt.sign(req.body,process.env.SECRET_KEY,(err,token)=>{
-                if(err) {
-                    res.json({"message":"This is wrong",err:err}).status(500)
-                }
-                else{
-                    res.json({
-                        Message: "Login Sucessfull",
-                        data : req.body,
-                        token:token
-                    })
-                }
-            })
-        }
-    })
-}
-catch(err){
-    res.json({"message":"This is error",err:err}).status(500)
-
-
-}
-}
-
-module.exports = {handleUserSignUp,handleUserLogin}
+module.exports = { handleUserSignUp, handleUserLogin, getdetails };
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,9 +1,11 @@
 var express = require('express')
-const {handleUserSignUp,handleUserLogin} =require('../Controllers/userController')
+const {handleUserSignUp,handleUserLogin, getdetails} =require('../Controllers/userController')
+const verify = require('../Middleware/verifyMiddleware');
 var router= express.Router();
 
 
 router.post('/sign',handleUserSignUp)
 router.post('/login',handleUserLogin)
+router.get('/getdetails',verify,getdetails)
 
 module.exports=router;
